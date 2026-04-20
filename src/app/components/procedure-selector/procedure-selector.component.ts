948c6dc0126b01dbb449576035266bcd6a68a6a6
YaanitGran <yaanit.granados@bunsan.io>
2026-04-19 18:55:19 -0600

    Se genera el código de procedure-selector y se integra en la página principal como un cambio de estado (no ruta) para evitar latencia

--- src/app/components/procedure-selector/procedure-selector.component.ts
+++ src/app/components/procedure-selector/procedure-selector.component.ts
@@ -1,13 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { 
+  IonGrid, IonRow, IonCol, IonCard, IonCardContent, 
+  IonIcon, IonButton 
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { 
+  documentTextOutline, bookOutline, personOutline, 
+  heartOutline, ribbonOutline, arrowBackOutline, closeOutline 
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-procedure-selector',
+  standalone: true,
   templateUrl: './procedure-selector.component.html',
   styleUrls: ['./procedure-selector.component.scss'],
+  imports: [
+    CommonModule, 
+    IonGrid, IonRow, IonCol, IonCard, IonCardContent, 
+    IonIcon, IonButton
+  ],
 })
 export class ProcedureSelectorComponent  implements OnInit {
+  @Input() title: string = '';
+  @Input() options: any[] = [];
+  @Output() onSelect = new EventEmitter<any>();
+  @Output() onBack = new EventEmitter<void>();
 
-  constructor() { }
+  constructor() {
+    addIcons({ 
+      documentTextOutline, bookOutline, personOutline, 
+      heartOutline, ribbonOutline, arrowBackOutline, closeOutline 
+    });
+  }
+  
+  selectOption(option: any) {
+    this.onSelect.emit(option);
+  }
+
+  goBack() {
+    this.onBack.emit();
+  }
 
   ngOnInit() {}
 
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -14,12 +14,12 @@ import {
  } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { 
-  documentTextOutline, 
-  folderOpenOutline, 
-  locationOutline 
+  documentTextOutline, folderOpenOutline, locationOutline, 
+  personOutline, heartOutline, ribbonOutline, bookOutline 
 } from 'ionicons/icons';
 import { HeaderComponent } from '../components/header/header.component';
 import { FooterComponent } from '../components/footer/footer.component';
+import { ProcedureSelectorComponent } from '../components/procedure-selector/procedure-selector.component';
 
 
 @Component({
@@ -41,11 +41,19 @@ import { FooterComponent } from '../components/footer/footer.component';
     IonCardContent,
     HeaderComponent,
     FooterComponent,
+    ProcedureSelectorComponent,
   ],
 })
 export class HomePage {
+
+  viewState: string = 'main';
+  
+  /** * Properties to pass data to the procedure-selector component */
+  selectorTitle: string = '';
+  currentOptions: any[] = [];
+
   constructor() {
-    addIcons({documentTextOutline,folderOpenOutline,locationOutline,});
+    addIcons({documentTextOutline,folderOpenOutline,locationOutline, personOutline,heartOutline,ribbonOutline,bookOutline});
   }
   /**
    * @param procedure Type of civil registry procedure selected by the user.
@@ -59,7 +67,69 @@ export class HomePage {
    * @param view The name of the target view
    */
   navigateTo(view: string) {
-    console.log('Navigating to:', view);
-    // Logic to switch components will go here
+    if (view === 'certified-copy') {
+      this.selectorTitle = 'Solicite su Acta Certificada';
+      this.currentOptions = [
+        { 
+          id: 1, 
+          label: 'Extracto de Acta', 
+          description: 'Copia rápida con datos esenciales del registro.', 
+          icon: 'document-text-outline', 
+          color: '#3b5998' 
+        },
+        { 
+          id: 2, 
+          label: 'Copia del Libro', 
+          description: 'Imagen fiel y completa del acta original.', 
+          icon: 'book-outline', 
+          color: '#3b5998' 
+        }
+      ];
+      this.viewState = 'selector';
+    } 
+    else if (view === 'procedures') {
+      this.selectorTitle = 'Catálogo de Trámites';
+      this.currentOptions = [
+        { 
+          id: 10, 
+          label: 'Nacimiento', 
+          description: 'Requisitos para registro y actas de nacimiento.', 
+          icon: 'person-outline', 
+          color: '#2d5a4c' 
+        },
+        { 
+          id: 11, 
+          label: 'Matrimonio', 
+          description: 'Información para celebrar matrimonios civiles.', 
+          icon: 'heart-outline', 
+          color: '#2d5a4c' 
+        },
+        { 
+          id: 12, 
+          label: 'Defunción', 
+          description: 'Guía y documentos para actas de defunción.', 
+          icon: 'ribbon-outline', 
+          color: '#2d5a4c' 
+        }
+      ];
+      this.viewState = 'selector';
+    }
+    else if (view === 'locations') {
+      // Logic for locations will go here later
+      console.log('Navegando a ubicaciones...');
+    }
+  }
+
+  handleBack() {
+    this.viewState = 'main';
+  }
+
+  /**
+   * Logic to handle when a specific procedure is clicked in the selector
+   * @param option The selected procedure object
+   */
+  handleOptionSelected(option: any) {
+    console.log('Trámite seleccionado:', option.label);
+    // Here we will eventually change viewState to 'info' to show requirements
   }
 }
